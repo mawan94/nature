9184220a5b31081ce44d54325efaf4705e3cc1f9
root <17521188586@163.com>
2018-08-30 22:33:56 +0800

    change banner componemt

--- nature/src/home/components/banner.js
+++ nature/src/home/components/banner.js
@@ -1,36 +1,29 @@
 import React, {Component} from 'react'
+import ReactSwipe from 'react-swipe';
 
 import head1 from '../../images/banner/head-1.png'
 import head2 from '../../images/banner/head-2.png'
 import head4 from '../../images/banner/head-4.png'
+
 require('../layout.css');
 
 export class Banner extends Component {
 
     render() {
-
         return (
-            <div id="carousel-example-generic" className="carousel slide"
-                 data-ride="carousel">
 
-                <ol className="carousel-indicators">
-                    <li data-target="#carousel-example-generic" data-slide-to="0" className="active"/>
-                    <li data-target="#carousel-example-generic" data-slide-to="1"/>
-                    <li data-target="#carousel-example-generic" data-slide-to="2"/>
-                </ol>
-
-                <div className="carousel-inner  banner-container" role="listbox">
-                    <div className="item active">
-                        <img className='banner-image' src={head4}/>
-                    </div>
-                    <div className="item">
-                        <img className='banner-image' src={head2}/>
-                    </div>
-                    <div className="item">
-                        <img className='banner-image' src={head1}/>
-                    </div>
+            <ReactSwipe className="carousel banner-container"
+                        swipeOptions={{continuous: true,auto:2600}}>
+                <div className="item active">
+                    <img className='banner-image' src={head4}/>
                 </div>
-            </div>
+                <div className="item">
+                    <img className='banner-image' src={head2}/>
+                </div>
+                <div className="item">
+                    <img className='banner-image' src={head1}/>
+                </div>
+            </ReactSwipe>
         )
     }
 }
